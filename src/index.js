e40548b364561843387dbd4afb1a73b3a3e3123a
Ryland Taniguchi <ryland0000@gmail.com>
2022-05-30 23:56:32 -0700

    noSwaps optimization

--- src/index.js
+++ src/index.js
@@ -46,17 +46,24 @@
 // 18.  Can you see it at a glance?
 // 19.  Can you make the code DRYer and refactor?
 // 20.  Can you improve the performance?
+
+// optimize if there was no swaps in the last iteration
+
 // 21.   How have other people solved this problem?
 
 function bubbleSort(arr) {
+  let noSwaps;
   for (let i = arr.length; i > 0; i--) {
+    noSwaps = true;
     for (let j = 0; j < i - 1; j++) {
       if (arr[j] > arr[j + 1]) {
         let temp = arr[j];
         arr[j] = arr[j + 1];
         arr[j + 1] = temp;
+        noSwaps = false;
       }
     }
+    if (noSwaps) break;
   }
   return arr;
 }
