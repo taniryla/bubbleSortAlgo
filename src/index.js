11f2742470d03c92e45ae978377f857880d591a7
Ryland Taniguchi <ryland0000@gmail.com>
2022-05-30 23:43:40 -0700

    update to j++

--- src/index.js
+++ src/index.js
@@ -50,7 +50,7 @@
 
 function bubbleSort(arr) {
   for (let i = arr.length; i > 0; i--) {
-    for (let j = 0; j < i - 1; i++) {
+    for (let j = 0; j < i - 1; j++) {
       if (arr[j] > arr[j + 1]) {
         let temp = arr[j];
         arr[j] = arr[j + 1];
